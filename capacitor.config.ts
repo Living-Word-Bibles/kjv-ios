c55565830c12a1bc8173806b2201008f15d2b2c7
Living-Word-Bibles <gospel@livingwordbibles.com>
2025-11-03 19:01:11 -0500

    Delete capacitor.config.ts

--- capacitor.config.ts
+++
@@ -1,9 +0,0 @@
-import { CapacitorConfig } from '@capacitor/cli';
-
-const config: CapacitorConfig = {
-  appId: 'com.lwb.bible',     // bundle id (say the word if you want a different one)
-  appName: 'LWB',             // app display name
-  webDir: 'www',
-  server: { iosScheme: 'https' } // keeps ATS happy
-};
-export default config;
